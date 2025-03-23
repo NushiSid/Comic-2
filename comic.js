9ae2b815dab75a9fd5f9d6bcf71e8f134d1aa925
NushiSid <puja.ramani@gmail.com>
2025-03-23 11:54:29 -0700

    Add files via upload

--- comic.js
+++ comic.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent('Caspian',{
+AFRAME.registerComponent('caspian',{
     schema:{
         pages:{type:'array', default:[
             {
@@ -7,31 +7,51 @@ AFRAME.registerComponent('Caspian',{
             },
             {
                 page:'ava',
-                color:"#d19cff"
+                color:"#aaf3fa"
             },  
             {
                 page:'rosie',
-                color:"#d19cff"
+                color:"#faaaf9"
             },
             {
                 page:'ella',
-                color:"#d19cff"
+                color:"#aafad9"
             },
             {
                 page:'luna',
-                color:"#d19cff"
+                color:"#aac5fa"
             }
         ]},
         pageIndex:{type:'int', default:0}
     },
     init:function(){
         var pageEl= this.pageEl=document.querySelector('[layer]');
-        pageEl.object3d.position.set(0, 1.8, -2.5);
+        pageEl.object3D.position.set(0, 1.8, -2.5);
         var pageIndex=this.data.pageIndex;
         window.addEventListener("keydown",(e) => {
-            if(e.key==="ArrowRight" & pageIndex<4)
+            if(e.key==="ArrowRight" & pageIndex<4){
                 pageIndex+=1;
                 this.turnPage(pageIndex);
-        })
-    }
+            }
+            if(e.key==="ArrowLeft" & pageIndex>=1){
+                pageIndex-=1;
+                this.turnPage(pageIndex);
+            }
+
+            
+
+        });
+
+    },
+    turnPage:function(pageIndex) {
+        var pages=this.data.pages;
+        var pageId=pages[pageIndex].page;
+        var pageColor=pages[pageIndex].color;
+        this.pageEl.setAttribute ('layer', 'src', '#' + pageId)
+        this.el.sceneEl.setAttribute ('background', 'color', pageColor)
+        
+
+    }       
+
 })
+
